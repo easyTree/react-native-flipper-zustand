7ef5b6ac3ada12f240faa88eb4b371d008237cd7
easyTree <easyTree@gmail.com>
2023-05-01 19:12:27 -0400

    Update index.d.ts
    // Commented code to fix issues with zustand v4, in-line with this comment:
    // https://github.com/cmdominguez/react-native-flipper-zustand/issues/4

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,51 @@
+// Commented code to fix issues with zustand v4, in-line with this comment:
+// https://github.com/cmdominguez/react-native-flipper-zustand/issues/4
+
 import {
-  Destroy,
-  GetState,
-  Mutate,
+//   Destroy,
+//   GetState,
+//   Mutate,
   State,
+  StateCreator,
   StoreMutatorIdentifier,
-  Subscribe,
+//   Subscribe,
 } from "zustand";
 
-export declare type SetState<T extends State> = {
-  _(
-    partial: T | Partial<T> | ((state: T) => T | Partial<T>),
-    replace?: boolean | undefined,
-    name?: string
-  ): void;
-}["_"];
+// export declare type SetState<T extends State> = {
+//   _(
+//     partial: T | Partial<T> | ((state: T) => T | Partial<T>),
+//     replace?: boolean | undefined,
+//     name?: string
+//   ): void;
+// }["_"];
 
-declare type StoreApi<T extends State> = {
-  setState: SetState<T>;
-  getState: GetState<T>;
-  subscribe: Subscribe<T>;
-  destroy: Destroy;
-};
+// declare type StoreApi<T extends State> = {
+//   setState: SetState<T>;
+//   getState: GetState<T>;
+//   subscribe: Subscribe<T>;
+//   destroy: Destroy;
+// };
 
-declare type Get<T, K, F = never> = K extends keyof T ? T[K] : F;
-declare type StateCreator<
-  T extends State,
-  Mis extends [StoreMutatorIdentifier, unknown][] = [],
-  Mos extends [StoreMutatorIdentifier, unknown][] = [],
-  U = T
-> = ((
-  setState: Get<Mutate<StoreApi<T>, Mis>, "setState", undefined>,
-  getState: Get<Mutate<StoreApi<T>, Mis>, "getState", undefined>,
-  store: Mutate<StoreApi<T>, Mis>,
-  $$storeMutations: Mis
-) => U) & {
-  $$storeMutators?: Mos;
-};
+// declare type Get<T, K, F = never> = K extends keyof T ? T[K] : F;
+// declare type StateCreator<
+//   T extends State,
+//   Mis extends [StoreMutatorIdentifier, unknown][] = [],
+//   Mos extends [StoreMutatorIdentifier, unknown][] = [],
+//   U = T
+// > = ((
+//   setState: Get<Mutate<StoreApi<T>, Mis>, "setState", undefined>,
+//   getState: Get<Mutate<StoreApi<T>, Mis>, "getState", undefined>,
+//   store: Mutate<StoreApi<T>, Mis>,
+//   $$storeMutations: Mis
+// ) => U) & {
+//   $$storeMutators?: Mos;
+// };
 
-type PopArgument<T extends (...a: never[]) => unknown> = T extends (
-  ...a: [...infer A, infer _]
-) => infer R
-  ? (...a: A) => R
-  : never;
+// type PopArgument<T extends (...a: never[]) => unknown> = T extends (
+//   ...a: [...infer A, infer _]
+// ) => infer R
+//   ? (...a: A) => R
+//   : never;
 
 type Logger = <
   T extends State,
